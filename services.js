9680b89741894b4ffff0acdd5ab1697ff6cceee1
GDiana <dcquinche@gmail.com>
2022-10-26 16:46:55 -0500

    Add the two functions to obtain all characters and episodes

--- services.js
+++ services.js
@@ -1,7 +1,24 @@
 const axios = require('axios');
 
+function getCharacters (){
+    axios.get('https://rickandmortyapi.com/api/character')
+  .then(function(res) {
+    console.log(res.data.results) 
+  })
+  .catch(function(error){
+    console.error(error)
+  })
+}
 
-
+function getEpisodes (){
+    axios.get('https://rickandmortyapi.com/api/episode')
+  .then(function(res) {
+    console.log(res.data.results) 
+  })
+  .catch(function(error){
+    console.error(error)
+  })
+}
 
 
 
